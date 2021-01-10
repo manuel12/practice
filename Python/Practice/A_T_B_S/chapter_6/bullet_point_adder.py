7c19ebae95876c439be9cd0c14e54c16b4f056d3
Manuel <manuel@softgames.de>
2021-01-10 18:25:25 +0100

    HTML & CSS further commit

--- Python/Practice/A_T_B_S/chapter_6/bullet_point_adder.py
+++
@@ -1,8 +0,0 @@
-import pyperclip
-text = pyperclip.paste()
-
-# Separate lines and add stars.
-lines = text.split('\n')
-lines = ['* ' + lines[i]  for i in  range(len(lines))]
-text = '\n'.join(lines)
-pyperclip.copy(text)
